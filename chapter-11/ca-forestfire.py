1ebd61859c358e61384c6a64672090fe0467c0e1
Carl Scheffler <carl.scheffler@gmail.com>
2019-01-17 16:12:30 +0200

    Added forest fire CA.

---
+++ chapter-11/ca-forestfire.py
@@ -0,0 +1,67 @@
+# Simple CA simulator in Python
+#
+# *** Forest fire ***
+#
+# Copyright 2008-2012 Hiroki Sayama
+# sayama@binghamton.edu
+
+# Modified to run with Python 3
+
+import matplotlib
+matplotlib.use('TkAgg')
+
+import pylab as PL
+import random as RD
+import scipy as SP
+
+RD.seed()
+
+width = 100
+height = 100
+initProb = 0.4
+empty, tree, fire, char = range(4)
+
+def init():
+    global time, config, nextConfig
+
+    time = 0
+
+    config = SP.zeros([height, width])
+    for x in range(width):
+        for y in range(height):
+            if RD.random() < initProb:
+                state = tree
+            else:
+                state = empty
+            config[y, x] = state
+    config[height//2, width//2] = fire
+
+    nextConfig = SP.zeros([height, width])
+
+def draw():
+    PL.cla()
+    PL.pcolor(config, vmin = 0, vmax = 3, cmap = PL.cm.binary)
+    PL.axis('image')
+    PL.title('t = ' + str(time))
+
+def step():
+    global time, config, nextConfig
+
+    time += 1
+
+    for x in range(width):
+        for y in range(height):
+            state = config[y, x]
+            if state == fire:
+                state = char
+            elif state == tree:
+                for dx in range(-1, 2):
+                    for dy in range(-1, 2):
+                        if config[(y+dy)%height, (x+dx)%width] == fire:
+                            state = fire
+            nextConfig[y, x] = state
+
+    config, nextConfig = nextConfig, config
+
+import pycxsimulator
+pycxsimulator.GUI().start(func=[init,draw,step])
