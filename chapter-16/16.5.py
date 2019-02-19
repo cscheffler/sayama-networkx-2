804f5d565e15ec43b1b60757dcac7a704cf39d99
Josh Broomberg <joshbroomberg@gmail.com>
2019-02-19 14:28:23 -0300

    Fix bug in 16.5

--- chapter-16/16.5.py
+++ chapter-16/16.5.py
@@ -9,7 +9,7 @@ def initialize():
     g = nx.karate_club_graph()
     g.pos = nx.spring_layout(g)
     for i in g.nodes:
-        g.nodes[i]['state'] = 1 if random() < .5 else 0
+        g.nodes[i]['state'] = 1 if rd.random() < .5 else 0
 
 def observe():
     global g
